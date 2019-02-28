aa3eab3cd565945ee6388a36226c39b8568fc2d1
QuinShuai <1127353554@qq.com>
2019-02-28 15:46:45 +0800

    修改一些代码规范

--- Assets/XAsset/Utility.cs
+++ Assets/XAsset/Utility.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -7,22 +8,20 @@ namespace XAsset {
     public class Utility {
         public const string AssetBundlesOutputPath = "AssetBundles";
 
-
-#if UNITY_EDITOR 
-        static int activeBundleMode = -1;
-        const string kActiveBundleMode = "ActiveBundleMode";
-
+#if UNITY_EDITOR
+        private static int _activeBundleMode = -1;
+        private const string ActiveBundleModeStr = "ActiveBundleMode";
         public static bool ActiveBundleMode {
             get {
-                if (activeBundleMode == -1)
-                    activeBundleMode = EditorPrefs.GetBool(kActiveBundleMode, true) ? 1 : 0;
-                return activeBundleMode != 0;
+                if (_activeBundleMode == -1)
+                    _activeBundleMode = EditorPrefs.GetBool(ActiveBundleModeStr, true) ? 1 : 0;
+                return _activeBundleMode != 0;
             }
             set {
                 int newValue = value ? 1 : 0;
-                if (newValue != activeBundleMode) {
-                    activeBundleMode = newValue;
-                    EditorPrefs.SetBool(kActiveBundleMode, value);
+                if (newValue != _activeBundleMode) {
+                    _activeBundleMode = newValue;
+                    EditorPrefs.SetBool(ActiveBundleModeStr, value);
                 }
             }
         }
@@ -37,55 +36,49 @@ namespace XAsset {
         }
 
         public static string GetPlatformForAssetBundles(RuntimePlatform platform) {
-            if (platform == RuntimePlatform.Android) {
-                return "Android";
-            }
-            if (platform == RuntimePlatform.IPhonePlayer) {
-                return "iOS";
-            }
-            if (platform == RuntimePlatform.tvOS) {
-                return "tvOS";
-            }
-            if (platform == RuntimePlatform.WebGLPlayer) {
-                return "WebGL";
-            }
-            if (platform == RuntimePlatform.WindowsPlayer || platform == RuntimePlatform.WindowsEditor) {
-                return "Windows";
-            }
-            if (platform == RuntimePlatform.OSXPlayer || platform == RuntimePlatform.OSXEditor) {
-                return "OSX";
+            switch (platform) {
+                case RuntimePlatform.Android:
+                    return "Android";
+                case RuntimePlatform.IPhonePlayer:
+                    return "iOS";
+                case RuntimePlatform.tvOS:
+                    return "tvOS";
+                case RuntimePlatform.WebGLPlayer:
+                    return "WebGL";
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.WindowsEditor:
+                    return "Windows";
+                case RuntimePlatform.OSXPlayer:
+                case RuntimePlatform.OSXEditor:
+                    return "OSX";
             }
             return null;
         }
 
 #if UNITY_EDITOR
         static string GetPlatformForAssetBundles(BuildTarget target) {
-            if (target == BuildTarget.Android) {
-                return "Android";
-            }
-            if (target == BuildTarget.tvOS) {
-                return "tvOS";
-            }
-            if (target == BuildTarget.iOS) {
-                return "iOS";
-            }
-            if (target == BuildTarget.WebGL) {
-                return "WebGL";
-            }
-            if (target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64) {
-                return "Windows";
-            }
-            if
+
+            switch (target) {
+                case BuildTarget.Android:
+                    return "Android";
+                case BuildTarget.iOS:
+                    return "iOS";
+                case BuildTarget.tvOS:
+                    return "tvOS";
+                case BuildTarget.WebGL:
+                    return "WebGL";
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    return "Windows";
 #if UNITY_2017_3_OR_NEWER
-            (target == BuildTarget.StandaloneOSX)
+                case BuildTarget.StandaloneOSX:
 #else
-            (target == BuildTarget.StandaloneOSXIntel || target ==  BuildTarget.StandaloneOSXIntel64 || target == BuildTarget.StandaloneOSX)
+                case BuildTarget.StandaloneOSXIntel:
+                case BuildTarget.StandaloneOSXIntel64:
+                case BuildTarget.StandaloneOSX:
 #endif
-            {
-                return "OSX";
+                    return "OSX";
             }
-            // Add more build targets for your own.
-            // If you add more targets, don't forget to add the same platforms to GetPlatformForAssetBundles(RuntimePlatform) function.
             return null;
         }
 #endif
